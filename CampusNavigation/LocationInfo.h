5cefd86bc69b79123653304af3f01facdf1404a9
Susan Song <2231443287@qq.com>
2026-06-20 12:11:57 +0800

    +第一步：简单构造LocationInfo.h

--- CampusNavigation/LocationInfo.h
+++ CampusNavigation/LocationInfo.h
@@ -10,15 +10,15 @@
 #include <utility>
 
 namespace Graph {
-    struct LocationInfo {
+    struct LocationInfo {                                   //Info就是Information的意思
         std::string place_id;       // 唯一ID，如 P0001
         std::string display_name;   // 展示名称
         std::string category;       // 类别，如 Teaching, Dining, Dormitory, Sports, Medical, Other
         int stay_time{0};           // 建议停留时间（单位：分钟）
         std::string open_time;      // 开放时间，如 08:00
         std::string close_time;     // 关闭时间，如 22:00
-
-        LocationInfo() = default;
+        
+        LocationInfo() = default;       //这是显式要求编译器生成默认构造函数。        //作用：告诉编译器“用你自动生成的那个无参构造函数就行，别因为我写了其他构造函数就不生成了”。
 
         LocationInfo(std::string place_id_, std::string display_name_,
                      std::string category_, int stay_time_,
